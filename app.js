03a5ce8f95d651a887584d33c902ebdb287d7c40
scorvov <scorvov@gmail.com>
2019-09-14 19:01:09 +0300

    ForHeroku0.6

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 var corsOptions = {
-  origin: 'https://rent-car-front.herokuapp.com/',
+  origin: true,
   credentials: true
 };
 
